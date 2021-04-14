6a32734f37fa3cf878c21ff54770a4534594920d
Ulises-Gordillo-1999 <uligordillo@gmail.com>
2021-04-14 02:27:29 -0300

    commit "URL_viaje en .json"

--- js/function.js
+++ js/function.js
@@ -2,10 +2,10 @@
 function crear_componente(viaje){
         return  `<div class="col-lg-4 col-md-6 mb-4">
                     <div class="card h-100">
-                    <a href="#" id="a_turist_KJ"><img class="card-img-top" src="${viaje.imagen}" alt=""></a>
+                    <a href="${viaje.URL_viaje}" id="a_turist_KJ" "><img class="card-img-top" src="${viaje.imagen}" alt=""></a>
                     <div class="card-body">
                         <h4 class="card-title">
-                        <a href="#" id="viaje1">${viaje.nombre}</a>
+                        <a href="${viaje.URL_viaje}" id="viaje1">${viaje.nombre}</a>
                         </h4>
                         <h5>${viaje.precio}</h5>
                         <p class="card-text" id="p_turist_KJ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
