38d5eda77bd2586ebab34f94aea14bc87744262a
Ulises-Gordillo-1999 <uligordillo@gmail.com>
2021-04-16 18:02:25 -0300

    commit proyecto

--- js/function.js
+++ js/function.js
@@ -2,7 +2,7 @@
 function crear_componente(viaje){
         return  `<div class="col-lg-4 col-md-6 mb-4">
                     <div class="card h-100">
-                    <a href="${viaje . URL}" id="a_turist_KJ" "><img class="card-img-top" src="${viaje.imagen}" alt=""></a>
+                    <img id="${viaje.idimg}" class="card-img-top" src="${viaje.imagen}" alt="">
                     <div class="card-body">
                         <h4 class="card-title">
                         <a href="${viaje . URL}" id="viaje1">${viaje.nombre}</a>
